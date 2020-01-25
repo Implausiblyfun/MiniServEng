a0f51f9b0c4642fe58beb9e8b2830cfb7aded27a
unknown <cyreal123@gmail.com>
2020-01-25 10:27:39 -0500

    Update yml to yaml so it actually works on appengine.

--- main.go
+++ main.go
@@ -6,23 +6,22 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 	"github.com/implausiblyfun/miniserveng/roomrouter"
 )
 
 func main() {
 	r := chi.NewRouter()
-	r.Use(middleware.Throttle(3))
+	// r.Use(middleware.Throttle(3))
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Plz dont overload this.\nJust trying to make a nice little easy webbysite."))
 	})
 
 	r.Route("/game", roomrouter.SetGameRoutes())
 
-	port := "970"
+	port := "8080"
 	fmt.Printf("Running on port %s\n", port)
 
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	if err := http.ListenAndServe(":"+port, r); err != nil {
 		log.Fatal(err)
 	}
 
